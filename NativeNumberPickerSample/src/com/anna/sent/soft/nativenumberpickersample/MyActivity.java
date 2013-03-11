92fb7a44fd2ce85dde0699408cb366d7fe8e3172
anna sent <anna.sent@gmail.com>
2013-03-11 16:16:07 +0400

    styles

--- NativeNumberPickerSample/src/com/anna/sent/soft/nativenumberpickersample/MyActivity.java
+++ NativeNumberPickerSample/src/com/anna/sent/soft/nativenumberpickersample/MyActivity.java
@@ -9,13 +9,9 @@ public class MyActivity extends Activity {
 		NumberPicker np1 = (NumberPicker) findViewById(R.id.numberPicker1);
 		np1.setMaxValue(20);
 		np1.setMinValue(0);
-		np1.setFocusable(true);
-		np1.setFocusableInTouchMode(true);
 
 		NumberPicker np2 = (NumberPicker) findViewById(R.id.numberPicker2);
 		np2.setMaxValue(20);
 		np2.setMinValue(0);
-		np2.setFocusable(true);
-		np2.setFocusableInTouchMode(true);
 	}
 }
