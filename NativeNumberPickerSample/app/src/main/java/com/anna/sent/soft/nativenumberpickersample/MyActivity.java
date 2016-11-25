cdbd2b858204d01fd811273556ccae371b90c9fd
Anna-Sentyakova <anna.sent@gmail.com>
2016-11-25 19:07:17 +0400

    migrate to Android Studio

---
+++ NativeNumberPickerSample/app/src/main/java/com/anna/sent/soft/nativenumberpickersample/MyActivity.java
@@ -0,0 +1,28 @@
+package com.anna.sent.soft.nativenumberpickersample;
+
+import android.os.Bundle;
+import android.widget.NumberPicker;
+
+import android.app.Activity;
+
+public class MyActivity extends Activity {
+	public final static String EXTRA_THEME_ID = "themeId";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(getIntent().getIntExtra(EXTRA_THEME_ID, 0));
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity);
+		setNumberPickerProperties();
+	}
+
+	private void setNumberPickerProperties() {
+		NumberPicker np1 = (NumberPicker) findViewById(R.id.numberPicker1);
+		np1.setMaxValue(20);
+		np1.setMinValue(0);
+
+		NumberPicker np2 = (NumberPicker) findViewById(R.id.numberPicker2);
+		np2.setMaxValue(20);
+		np2.setMinValue(0);
+	}
+}
